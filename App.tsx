282da08adf4cd9194a4c489e21e9c80213fc4c2e
김다운 <gimdaun@gimdauns-MacBook-Pro.local>
2023-01-26 19:15:57 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>
+        시발
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
