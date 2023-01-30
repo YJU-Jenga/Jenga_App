f5b6857dbea5e746c50f5bf6890c9be3a6c383c2
김다운 <gimdaun@gimdauns-MacBook-Pro.local>
2023-01-30 15:16:41 +0900

    make Homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,16 +6,20 @@ import React from 'react';
 const HomeScreen = () => {
   return (
     <View style={{ flex: 1, paddingTop: 10, justifyContent: 'center', alignContent: 'center', backgroundColor: 'white' }}>
-      <Text style={{ fontSize: 50 }}>
-          🍓
-      </Text>
-      <Text style={{ fontSize: 50, fontWeight: 'bold' }}>
-          안녕 다운!
-      </Text>
-      <Image source={require('../assets/image/mic2.png')} style={{ width: 100, height: 100, borderRadius: 60 }}/>
-      <Text style={{ fontSize: 20 }}>
-          인형 이름을 말해주세요
-      </Text>
+      <View style={{ paddingBottom: 200 }}>
+        <Text style={{ fontSize: 50, textAlign: 'center', paddingBottom: 20 }}>
+            🍓
+        </Text>
+        <Text style={{ fontSize: 50, fontWeight: 'bold', textAlign: 'center' }}>
+            안녕 다운!
+        </Text>
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <Image source={require('../assets/image/mic2.png')} style={{ width: 100, height: 100, borderRadius: 60, padding: 40 }}/>
+        <Text style={{ fontSize: 20, textAlign: 'center' }}>
+            인형 이름을 말해주세요
+        </Text>
+      </View>
     </View>
   );
 };
